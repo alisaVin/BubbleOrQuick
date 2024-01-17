0e4088f7918a638c6817360fede81a84b949b3fd
Alisa Vinogradova <avinogradova@kesslersolutions.de>
2024-01-17 14:16:31 +0100

    initial commit

---
+++ Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace QuickOrBubbleSort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {   
+            //Random input
+            Console.WriteLine("Wie viel Elemente sollen im Array sein? : ");
+            int numberInput = Convert.ToInt32(Console.ReadLine());
+            int[] numbersArr = new int[numberInput];
+
+            Console.Write("So sieht das Array aus: [");
+            Random rnd = new Random();
+
+            for (int item = 0; item < numberInput; item++)
+            {
+                numbersArr[item] = rnd.Next(1000);
+                Console.Write($"{numbersArr[item]} ");
+            }
+            Console.WriteLine("\b]");
+            Console.WriteLine("----------------");
+            //Choise
+            Console.WriteLine("Bitte wählen Sie den Algorythmus, nach dem die Zahlen im Array sortiert werden sollen:");
+            Console.WriteLine();
+            Console.WriteLine("1. Bubble Sort");
+            Console.WriteLine("2. Quick Sort");
+            Console.WriteLine("----------------");
+
+            int inpChoise = Convert.ToInt32(Console.ReadLine());
+
+            if (inpChoise == 1)
+            {
+                Console.Write("So sieht das nach der Bubble-Sorstierung aus: [");
+                Bubble bubble1 = new Bubble();
+                bubble1.BSort(numbersArr);
+            }
+            else if (inpChoise == 2)
+            {
+                Console.Write("So sieht das nach der Quick-Sorstierung aus: [");
+                Quick quick1 = new Quick();
+                quick1.QSort(numbersArr); 
+            }
+            else
+            {
+                Console.WriteLine("_(:3_z)_");
+            }
+            
+            Console.WriteLine("\b]");
+            Console.WriteLine("****************");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Quick.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+public class Quick 
+{
+	public void QSort(int[] numbersArr)
+	{
+		int len = numbersArr.Length;
+		int pivot_1 = numbersArr[0];                           //Pivot am Anfang                                
+		int m;                                                 //Median Ind *
+
+		//Median berechnen
+		if (len % 2 == 1)
+		{
+			m = (len - 1) / 2;
+        }
+		else 
+		{
+            m = (len / 2) + 1; 
+        }
+        numbersArr[0] = numbersArr[m];
+		numbersArr[m] = pivot_1;
+
+        //allgemein muss noch Fehler behebt werden (pivot schauen)
+        //1 ----> 
+        for (int i = 0; i < m; i++)
+		{
+			if (numbersArr[i] > pivot_1)
+			{
+                int more = numbersArr[i];
+				numbersArr[i] = numbersArr[m + 1];
+                numbersArr[m + 1] = more;
+            }
+		}
+		//1 <----
+		for (int j = len - 1; j > m; j--)
+		{
+			if (numbersArr[j] < pivot_1)
+			{
+                int less = numbersArr[j];
+                numbersArr[j] = numbersArr[m - 1];
+                numbersArr[m - 1] = less;
+            }
+		}
+
+		//2 <----
+		//int pivot_2 = 
+        foreach (int num in numbersArr)
+        {
+            Console.Write($"{num} ");
+        }
+    }
+}
