b500f131ed2c126c17bab07e93f4b40fd2ab599c
Alisa Vinogradova <avinogradova@kesslersolutions.de>
2024-01-24 11:45:50 +0100

    end

---
+++ Bubble.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace QuickOrBubbleSort
+{
+    public class Bubble
+    {
+        public void BSort(int[] numbersArr)
+        {
+            for (int item = 0; item < numbersArr.Length; item++)
+            {
+                for (int sort = 0; sort < numbersArr.Length - 1; sort++)
+                {
+                    if (numbersArr[sort] > numbersArr[sort + 1])
+                    {
+                        int temp = numbersArr[sort];
+                        numbersArr[sort] = numbersArr[sort + 1];
+                        numbersArr[sort + 1] = temp;
+                    }
+                }
+            }
+            foreach (int item in numbersArr)
+            {
+                Console.Write($"{item} ");
+            }
+        }
+    }
+}
+
---
+++ Quick.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+//FINAL BEARBEITEN!!!
+class Quick
+{
+    public static int split(int[] numbersArr, int less, int more)
+    {
+        int i = less - 1;
+        int pivot = numbersArr[more];
+
+        for (int j = less; j <= more; j++)
+        {
+            if (numbersArr[j] < pivot)
+            {
+                i++;
+
+                int temp = numbersArr[i];
+                numbersArr[i] = numbersArr[j];
+                numbersArr[j] = temp;
+            }
+        }
+        i++;
+
+        int temp_2 = numbersArr[i];
+        numbersArr[i] = numbersArr[more];
+        numbersArr[more] = temp_2;
+
+        return i; 
+    }
+
+    public void QSort(int[] numbersArr, int less, int more)
+    {
+        if (more <= less) return; 
+
+        int pivot_i = split(numbersArr, less, more);
+        QSort(numbersArr, less, pivot_i - 1);
+        QSort(numbersArr, pivot_i + 1, more);
+    }
+}
